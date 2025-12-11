ee0e9828fc23bddb2748c564088fbc7c4ce09a70
MuhammedSadiqA <muhammedsadiq3669@gmail.com>
2025-12-11 12:59:13 +0530

    created multermiddleware

---
+++ controllerLogic/bookController.js
@@ -0,0 +1,11 @@
+const books=require('../models/bookModel')
+
+// add book
+exports.addBookController=async(req,res)=>{
+    console.log("Inside addBookController");
+    // get books details from req body
+    //const{title,author,pages,price,discountPrice,imageURL,abstract,language,publisher,isbn,category,uploadImages}=req.body
+    // console.log(title,author,pages,price,discountPrice,imageURL,abstract,language,publisher,isbn,category,uploadImages);
+    // res.status(200).json("Add book request received")
+      
+    }
--- routes/routing.js
+++ routes/routing.js
@@ -1,6 +1,8 @@
 // import express
 const express = require('express')
 const userController = require('../controllerLogic/userControllerLogic')
+const bookController =require('../controllerLogic/bookController')
+const jwtMiddleware = require('../middlewares/jwtMiddleware')
 
 // create Router object
 const router = express.Router()
@@ -12,5 +14,9 @@ router.post('/register',userController.registerController)
 router.post('/login',userController.logincontroller)
 // googlelogin
 router.post('/google/sign-in',userController.googleLogincontroller)
+
+// Authorised User
+// add book
+router.post('/user/book/add',jwtMiddleware,bookController.addBookController)
 
 module.exports = router
